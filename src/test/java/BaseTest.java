f0dbab3ba80120a0cb2ac0703131ff076b3c7003
VitaliiMalatok <palinamalatokpolish@gmail.com>
2025-05-13 17:53:16 +0200

    modify BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -15,28 +15,22 @@ public class BaseTest {
     public static final int TIMEOUT = 8;
     protected WebDriver driver;
     private static final Logger LOGGER = LogManager.getLogger(BaseTest.class);
-//    public TelegramBotRunner botRunner;
 
     @BeforeClass
     public void setUp() {
         LOGGER.info("Setting up WebDriver...");
         ChromeOptions options = new ChromeOptions();
-//        botRunner = new TelegramBotRunner();
         //options.addArguments(INCOGNITO);
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(TIMEOUT, TimeUnit.SECONDS);
         driver.get("https://mts.by/");
-//        driver.get("https://www.onliner.by/");
-
         LOGGER.info("Navigated to: " + "baseUrl");
     }
 
 
-
     @AfterClass
     public void tearDown() throws IOException {
-
         if (driver != null) {
             LOGGER.info("Closing WebDriver...");
             driver.quit();
